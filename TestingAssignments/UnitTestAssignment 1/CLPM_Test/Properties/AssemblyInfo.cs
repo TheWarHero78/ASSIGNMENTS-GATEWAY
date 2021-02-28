1d40dee39b8e9609cfafe4c62860cb7cab779afb
TheWarHero78 <54692481+TheWarHero78@users.noreply.github.com>
2021-02-28 09:06:12 +0530

    completed unit test assignment1 and 2

---
+++ TestingAssignments/UnitTestAssignment 1/CLPM_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CLPM_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CLPM_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("babe14fd-bb38-49c2-be85-5185313a0bf6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
